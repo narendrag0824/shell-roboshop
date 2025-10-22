a4f948a323d893ff21a9d2aed957517ee6eda559
narendra <narendrag0824@gmail.com>
2025-10-22 17:48:38 +0530

    shell roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ for instance in $@
 do
       instanceid=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-05ceca7471f660a07 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-if [ $instance _ne "frontend" ]; then
+if [ $instance != "frontend" ]; then
     ip=$(aws ec2 describe-instances --instance-ids $instanceid --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     recordname="$instance.$domainname"
 
