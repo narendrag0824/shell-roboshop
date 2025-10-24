5bb5c8a4205a016c6ddfeb9ea5ce4971c6dfff63
narendra <narendrag0824@gmail.com>
2025-10-24 16:49:15 +0530

    narendra

---
+++ redis.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+
+logfolder="/var/log/shell-roboshop"
+scriptname=$( echo $0 | cut -d "." -f1 )
+logfile="$logfolder/$scriptname.log"
+mongodbhost="mongodb.narendra.fun"
+script_dir="$(pwd)"
+
+mkdir -p $logfolder
+echo "script started excuted: $(date)"
+
+if [ $userid -ne 0 ]; then
+    echo -e "error::please run as sudo user"
+    exit 1
+fi
+
+validate(){
+  if [ $1 -ne 0 ]; then
+      echo -e "$2 ... $r is failed $n"
+      exit 1
+  else
+      echo -e "$2 ... $g is success $n"
+  fi      
+}
+
+dnf module disable redis -y &>>$logfile
+validate $? "disable redis"
+
+dnf module enable redis:7 -y &>>$logfile
+validate $? "enable redis"
+
+dnf install redis -y &>>$logfile
+validate $? "install redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+validate $? "change ip id"
+
+systemctl enable redis &>>$logfile
+validate $? "enable redis"
+
+systemctl start redis &>>$logfile
+validate $? "start redis"
